bd56f4cb832031f13d9bf0ae914420ec1a66be2b
Chris Havlin <chris.havlin@gmail.com>
2020-10-08 11:01:04 -0500

    fixed the merge

--- pckgs/check_tests.py
+++ pckgs/check_tests.py
@@ -5,7 +5,7 @@ import os
 import matplotlib.pyplot as plt 
 
 # 'https://tests.yt-project.org/job/yt_py38_git/1552/testReport/api/json?pretty=true'
-pr_tnums={2931:1552,2934:1568}
+pr_tnums={2931:1552,2934:1568,2928:1559,2937:1576}
 
 class test_result(object):
     def __init__(self,PR,testnum):
@@ -33,27 +33,31 @@ class test_result(object):
         self.df = pd.DataFrame(rows)
         
 def compare_two_tests(PR1,PR2,**kwargs):
-    PR_1 = test_result(PR1,pr_tnums[PR1])
-    PR_2 = test_result(PR2,pr_tnums[PR2])
+    PR_1 = test_result(PR1[0],PR1[1])
+    PR_2 = test_result(PR2[0],PR2[1])
     
-    cols = ['className','duration']
-    df1 = PR_1.df[cols].groupby(by=['className']).sum().reset_index()
-    df2 = PR_2.df[cols].groupby(by=['className']).sum().reset_index() 
-    df = pd.merge(df1,df2,on=['className'],how='left',suffixes=(f"_{PR1}", f"_{PR2}"))
+    cols = ['className','duration','name']
+    df1 = PR_1.df[cols].groupby(by=['className','name']).sum().reset_index()
+    df2 = PR_2.df[cols].groupby(by=['className','name']).sum().reset_index() 
+    df1 = df1[df1.duration!=0.]
+    df2 = df2[df2.duration!=0.]
+    df = pd.merge(df1,df2,on=['className','name'],how='inner',suffixes=(f"_{PR1}", f"_{PR2}"))
+
     df['timediff']=df[f"duration_{PR1}"]-df[f"duration_{PR2}"]
+    df['timeratio']=df[f"duration_{PR1}"]/df[f"duration_{PR2}"]
     
     plot = kwargs.pop('plot',True)
     if plot:
         bins = kwargs.pop('bins',100)
-        ax = df['timediff'].hist(bins=bins,**kwargs)        
-        ax.set_xlabel(f"PR {PR1} - PR {PR2} [s]")
+        ax = df['timeratio'].hist(bins=bins,**kwargs)        
+        ax.set_xlabel(f"PR {PR1} / PR {PR2}")
         ax.set_ylabel('N')
         plt.show() 
     return df 
     
 if __name__=='__main__':
     
-    PR2934 = ct.test_result(2934,pr_tnums[2934])
-    PR2931 = ct.test_result(2931,pr_tnums[2931])
+    PR2934 = test_result(2934,pr_tnums[2934])
+    PR2931 = test_result(2931,pr_tnums[2931])
 
         
